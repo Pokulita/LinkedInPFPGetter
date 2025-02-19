816a6964f303b79df43b6cb2e4c012f808d3d271
pokulita <nikitasl2208@gmail.com>
2025-02-19 04:27:48 +0100

    minor fixes

--- Dockerfile
+++ Dockerfile
@@ -4,24 +4,20 @@ WORKDIR /app
 
 COPY requirements.txt .
 
+RUN apt-get update && apt-get install -y \
+    wget curl unzip xvfb libxi6 libgconf-2-4 libdbus-glib-1-2 \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN apt-get update && apt-get install -y firefox-esr
+
+RUN wget -q "https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz" && \
+    tar -xzf geckodriver-v0.30.0-linux64.tar.gz -C /usr/bin/ && \
+    rm geckodriver-v0.30.0-linux64.tar.gz
+
+ENV FIREFOX_BIN="/usr/bin/firefox"
+ENV GECKODRIVER_BIN="/usr/bin/geckodriver"
+
 RUN pip install --no-cache-dir -r requirements.txt
-
-RUN apt-get update && apt-get install -y wget curl unzip xvfb libxi6 libgconf-2-4
-
-# Install Google Chrome
-RUN wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
-    && dpkg -i google-chrome-stable_current_amd64.deb || apt-get -fy install \
-    && rm google-chrome-stable_current_amd64.deb
-
-# Install ChromeDriver
-RUN CHROMEDRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE) && \
-    wget -q https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip && \
-    unzip chromedriver_linux64.zip -d /usr/bin/ && \
-    rm chromedriver_linux64.zip
-
-# Set environment variables for Selenium
-ENV CHROME_BIN="/usr/bin/google-chrome"
-ENV CHROMEDRIVER_BIN="/usr/bin/chromedriver"
 
 COPY . .
 
--- getter_script.py
+++ getter_script.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,23 +13,16 @@ logging.basicConfig(filename='out.log', level=logging.INFO,
 
 def get_linkedin_profile_image(username, password):
     try:
-        print(f"Password: {password}")
-        options = webdriver.ChromeOptions()
+        options = Options()
+
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
 
-
-        options.binary_location = "/usr/bin/google-chrome"
-
-        driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=options)
-
-        driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        driver = webdriver.Firefox( options=options)
 
         driver.get("https://www.linkedin.com/login")
 
-        print(f"Password: {password}")
         email_field = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "username"))
         )
@@ -42,8 +36,8 @@ def get_linkedin_profile_image(username, password):
         login_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "/html/body/div/main/div[3]/div[1]/form/div[4]/button"))
         )
-        login_button.click()
 
+        login_button.click()
         print("Logging in...")
 
         time.sleep(5)
